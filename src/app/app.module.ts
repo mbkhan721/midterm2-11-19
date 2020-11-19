27bd0157705c751dc8b8b3131a18dbc0186edb05
mbkhan721 <muhammad.khan33@student.qcc.cuny.edu>
2020-11-19 11:56:54 -0500

    1 file modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
 import { MyResumeComponent } from './my-resume/my-resume.component';
+import { ResumeEducationComponent } from './my-resume/resume-education/resume-education.component';
+import { ResumeFooterComponent } from './my-resume/resume-footer/resume-footer.component';
+import { ResumeProfileComponent } from './my-resume/resume-profile/resume-profile.component';
+import { ResumeWorkExperienceComponent } from './my-resume/resume-work-experience/resume-work-experience.component';
 
 @NgModule({
   imports: [
@@ -35,7 +39,11 @@ import { MyResumeComponent } from './my-resume/my-resume.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
-    MyResumeComponent
+    MyResumeComponent,
+    ResumeEducationComponent,
+    ResumeFooterComponent,
+    ResumeProfileComponent,
+    ResumeWorkExperienceComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
